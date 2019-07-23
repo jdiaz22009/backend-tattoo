cf9bfa1578245bb6554e8b30b093211358bc3fb9
juan diaz <juan_diaz@juans-MacBook-Pro.local>
2019-07-23 02:11:02 -0500

    config db local 3

--- src/db/db_auth.js
+++ src/db/db_auth.js
@@ -5,7 +5,7 @@ module.exports = {
         password: process.env.DB_PASS || '',
         host: process.env.DB_HOST || 'localhost',
         port: process.env.DB_PORT || '27017',
-        db: process.env.DBAUTHMONGO || 'mongodb://localhost/tatto-auth',
+        db: process.env.DBAUTHMONGO || 'mongodb+srv://admin:d3v3l0p3r@cluster0-dfj8d.mongodb.net/tatto-auth?retryWrites=true&w=majority',
     },
     server: {
         port: process.env.AUTHPORT || 3001,
