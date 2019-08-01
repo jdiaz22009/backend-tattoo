a54f33c9f74c357a65ec67cc01387bfc140d8b27
juan diaz <juan_diaz@juans-MacBook-Pro.local>
2019-07-31 23:09:57 -0500

    index public

---
+++ public/precache-manifest.6058c23729bddeaef1229da78f5b32dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a36dc1a5edfd3d7078023936da1818",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09b4d73a95890667b9ef",
+    "url": "/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "4441ff018774dcc29c30",
+    "url": "/static/css/main.fb82ace1.chunk.css"
+  },
+  {
+    "revision": "09b4d73a95890667b9ef",
+    "url": "/static/js/2.f99b9988.chunk.js"
+  },
+  {
+    "revision": "4441ff018774dcc29c30",
+    "url": "/static/js/main.8af8fe3a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const config = require('./config');
 const app = require('./app');
+const express = require('express')
+const path = require('path')
 const http_server = require('http').createServer(app)
 const log = console.log;
 
@@ -10,9 +12,11 @@ const portprod = config.server.portprod;
 if (env === 'dev') {
     http_server.listen(port,function(){
         log(`[tatto-server] listening on port http://localhost:${port}, in mode ${env}`);
+        app.use( express.static(path.join(__dirname,'../public')))
     });
 } else {
     http_server.listen(portprod,function(){
         log(`[tatto-server] listening on port http://localhost:${port}, in mode ${env}`);
+        app.use( express.static(path.join(__dirname,'../public')))
     });
 }
